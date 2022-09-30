0e73a15112da1a4f57108b7b1bb49afdbf0d4e8e
DA0C921 <julio20447@cbtis75.edu.mx>
2022-09-30 09:15:21 -0500

    30 sep

---
+++ Parcial2/hola.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<h1>';
+    echo 'Hola a todos :)';
+    echo '</h1>';
+    ?>
+    <style>
+      body{
+        background-color: black;
+      }
+      h1{
+        color: white;
+        font-size: 120PX;
+        text-align: center;
+      }
+    </style>
+</body>
+</html>
