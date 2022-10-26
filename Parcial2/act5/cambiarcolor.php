97d6dd045aab635f5dfc18351419d04175cee6aa
AlejandroGH <114587428+DA0C921@users.noreply.github.com>
2022-10-26 07:59:01 -0500

    Update cambiarcolor.php

--- Parcial2/act5/cambiarcolor.php
+++ Parcial2/act5/cambiarcolor.php
@@ -10,7 +10,7 @@
     <title>Actividad 4</title>
 </head>
 <body>
-    <h1 id="Samu">Soy una simple página!!! =( =( =(</h1>
+   <h1 id="Samu">Soy una simple página!!! =( =( =(</h1><!-- Al h1 le agregamos un id por que lo utilizaremos mas adelante    -->
     <button id="btnrojo" style="background-color: red ;" onmouseover="Rojo()">Red</button> <!-- Creeamos un boton y le damos un color en este caso rojo   -->
     <button id="btnazul" style="background-color: blue ;" onmouseover="Azul()">Blue</button><!-- Creeamos un boton y le damos un color en este caso azul   -->
     <button id="btnverde" style="background-color: green ;" onmouseover="Verde()">Green</button><!-- Creeamos un boton y le damos un color en este caso verde   -->
@@ -18,18 +18,18 @@
     <script>
         function Rojo(){
             //alert("Outchh Soy rojo");
-            document.getElementById("Samu").style.backgroundColor="red";
+       document.getElementById("Samu").style.backgroundColor="red";  //La funcion hace que al darle click al boton hace que el id "samu" que en este caso es el h1 cambie de color rojo
 
         }
         function Azul(){
             //alert("Outchh Soy Azul");
-            document.getElementById("Samu").style.backgroundColor="blue";
+            document.getElementById("Samu").style.backgroundColor="blue";//La funcion hace que al darle al darle click al boton hace que el id "samu" que en este caso es el h1 cambie de color azul
 
 
         }
         function Verde(){
             //alert("Outchh Soy verde");
-            document.getElementById("Samu").style.backgroundColor="green";
+            document.getElementById("Samu").style.backgroundColor="green";//La funcion hace que al darle al darle click al boton hace que el id "samu" que en este caso es el h1 cambie de color al verde
 
 
         }
